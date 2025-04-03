e49d959c102bc0936118abe026b649165a6848f7
KANE <netmorphus@hotmail.com>
2025-04-03 16:35:14 +0100

    Modification du fichier taxpayertaxRoutes.js

--- routes/taxpayertaxRoutes.js
+++ routes/taxpayertaxRoutes.js
@@ -1,14 +1,4 @@
-// const express = require("express");
-// const { getTaxesByTaxpayer } = require("../controllers/taxpayertaxController");
-// const authMiddleware = require("../middleware/authMiddleware");
-// const roleMiddleware = require("../middleware/roleMiddleware");
 
-// const router = express.Router();
-
-// // Route pour récupérer les taxes associées à un contribuable
-// router.get("/:taxpayerId", authMiddleware, roleMiddleware("collector"), getTaxesByTaxpayer);
-
-// module.exports = router;
 
 
 const express = require("express");
